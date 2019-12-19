26fd9007de8dd32c6d4037ca94b3b2c565d3614b
James McDermott <james.mcdermott7@mail.dcu.ie>
2019-12-19 20:11:45 +0000

    ADD: schedule parser with unit tests

---
+++ src/odin-engine/parser/parser.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "strings"
+)
+
+type StringFormat struct {
+    Minute string
+    Hour string
+    Dom string
+    Mon string
+    Dow string
+}
+
+func isTimeValid(time string, matchMe string, results []string) ([]string, float64) {
+    var addOn float64 = 0
+    timeSplit := splitOnKeyword(time, ":")
+    for _, ts := range timeSplit {
+        if compare(ts, matchMe) {
+            results = append(results, "0")
+            addOn = 0.5
+        }
+    }
+    return results, addOn
+}
+
+func isScheduleValid(schedule string) bool {
+    var results []string
+    var timeSplitAddOn float64 = 0
+    var addOn float64
+    words := strings.Fields(schedule)
+    for _, word := range words {
+        for _, valid := range strings.Fields(getValidKeywords()) {
+            if strings.Contains(word, ":") {
+                results, addOn = isTimeValid(word, valid, results)
+                timeSplitAddOn += addOn
+            } else {
+                if compare(valid, word) {
+                    results = append(results, "0")
+                    break
+                }
+            }
+        }
+    }
+    return len(results) == len(words)+int(timeSplitAddOn)
+}
+
+func getCron(values map[string]string, key string) string {
+    var result string
+    if values[key] == "" {
+        result = "*"
+    } else {
+        result = values[key]
+    }
+    return result
+}
+
+func getCronMonth(values map[string]string, currentDom string, key string) (string, string) {
+    var result string
+    newKey := strings.Join(strings.Split(key, " ")[0:2]," ")
+    if values[newKey] == "" {
+        result = "*"
+    } else {
+        result = values[newKey]
+        if currentDom == "*" {
+            currentDom = stripChars(strings.Split(key, " ")[len(strings.Split(key, " "))-1], "strfdh")
+        }
+    }
+    return result, currentDom
+}
+
+func main() {
+    dowValues := getDowMap()
+    domValues := getDomMap()
+    monValues := getMonMap()
+    var formattedRules [][]string
+    filename := os.Args[1]
+    yaml := getYaml(filename)
+
+    if isScheduleValid(yaml) {
+        for _, rule := range splitOnKeyword(yaml, "and") {
+            formattedRules = append(formattedRules, splitOnKeyword(rule, "at"))
+        }
+        for i, _ := range formattedRules {
+            var sf StringFormat
+            key := trimEdges(formattedRules[i][0], "\t \n")
+            sf.Dow = getCron(dowValues, key)
+            sf.Dom = getCron(domValues, key)
+            sf.Mon, sf.Dom = getCronMonth(monValues, sf.Dom, key)
+            sf.Hour, sf.Minute = splitOnKeyword(formattedRules[i][1], ":")[0], splitOnKeyword(formattedRules[i][1], ":")[1]
+            fmt.Println(sf.Minute, sf.Hour, sf.Dom, sf.Mon, sf.Dow)
+        }
+    } else {
+        fmt.Println("Odin cannot recognise the schedule found in your Yaml config file.")
+    }
+}
---
+++ src/odin-engine/parser/testConfigs/readme.txt
@@ -0,0 +1,5 @@
+This is a txt file.
+
+There isn't much to be found in here.
+
+That is all.
