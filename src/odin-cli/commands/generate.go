0867b846400e4eb632013dfcbc25612a4a69120b
James McDermott <james.mcdermott7@mail.dcu.ie>
2020-02-17 23:38:20 +0000

    ADD: generate command to create template job Yaml files

--- src/odin-cli/commands/generate.go
+++ src/odin-cli/commands/generate.go
@@ -2,6 +2,8 @@ package commands
 
 import (
     "fmt"
+    "io/ioutil"
+    "strings"
     "github.com/spf13/cobra"
 )
 
@@ -16,8 +18,18 @@ var GenerateCmd = &cobra.Command{
 
 func init() {
     RootCmd.AddCommand(GenerateCmd)
+    GenerateCmd.Flags().StringP("file", "f", "", "file (required)")
+    GenerateCmd.MarkFlagRequired("file")
 }
 
 func generateJob(cmd *cobra.Command, args []string) {
-    fmt.Println("generate job")
+    name, _:= cmd.Flags().GetString("file")
+    if strings.HasSuffix(name, ".yml") ||strings.HasSuffix(name, ".yaml") {
+        data := []byte("provider:\n  name: 'odin'\n  version: '1.0.0'\njob:\n  name: ''\n  description: ''\n  language: ''\n  file: ''\n  schedule: ''\n\n")
+        err := ioutil.WriteFile(name, data, 0644)
+        if err != nil {
+            panic(err)
+        }
+        fmt.Println(name + " config generated!")
+    }
 }
