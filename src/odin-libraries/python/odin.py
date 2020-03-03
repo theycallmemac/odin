1fb5efc402fb969f3a2f2717d8aefcb0768a8f8c
James McDermott <james.mcdermott7@mail.dcu.ie>
2020-03-03 16:12:36 +0000

    ADD: upsert functionality to python library

--- src/odin-libraries/python/odin.py
+++ src/odin-libraries/python/odin.py
@@ -1,17 +1,21 @@
-from os import environ
+from sys import exit
 from ruamel.yaml import YAML
 from odin_logger import OdinLogger as logger
 
 class Odin:
-    def __init__(self, config="job.yml"):
-        data = YAML().load(open(config,"r").read())
+    def __init__(self, config="job.yml", mongodb="mongodb://localhost:27017"):
+        self.config = config
+        data = YAML().load(open(self.config,"r").read())
         self.id = data["job"]["id"]
+        self.mongodb = mongodb
 
     def condition(self, desc, expr):
-        logger.log("condition", desc, expr, self.id)
+        logger.log("condition", desc, expr, self.id, self.mongodb)
         return expr
+    
     def watch(self, desc, value):
-        logger.log("watch", desc, value, self.id)
+        logger.log("watch", desc, value, self.id, self.mongodb)
 
     def result(self, desc, status):
-        logger.log("result", desc, status, self.id)
+        logger.log("result", desc, status, self.id, self.mongodb)
+        exit(0)
--- src/odin-libraries/python/odin_logger.py
+++ src/odin-libraries/python/odin_logger.py
@@ -1,20 +1,23 @@
-import motor.motor_asyncio
-import asyncio
-from os import environ
-
-# start mongodb driver client
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
-
-# get odin db
-db = client['odin']
+from pymongo import MongoClient
 
 class OdinLogger:
     @classmethod
-    def log(self, type, desc, value, id):
+    def log(self, type, desc, value, id, mongo):
+        # start mongodb driver client
+        client = MongoClient(mongo)
+        # get odin db
+        db = client['odin']
         collection = db['observability']
-        collection.insert_one({
-            'type' : str(type),
-            'desc' : str(desc),
-            'value' : str(value),
-            'id' : str(id),
-        })
+        self.find_and_insert(collection, type, desc, value,  id)
+
+    def find_and_insert(collection, type, desc, value, id):
+        collection.update(
+            {'id': str(id), 'desc':str(desc), 'type': str(type)},
+            {'$set' : {
+                'type' : str(type),
+                'desc' : str(desc),
+                'value' : str(value),
+                'id' : str(id),
+            }}, upsert=True
+        )
+
