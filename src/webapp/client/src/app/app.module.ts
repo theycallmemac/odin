bb3264bb408ade0f90ee92b382386c8980dcbbef
Martynas <martynas@outlook.com>
2020-03-23 20:19:19 +0100

    ADD: Add previously commited components to app
    - adds components so they could be used in app

--- src/webapp/client/src/app/app.module.ts
+++ src/webapp/client/src/app/app.module.ts
@@ -16,6 +16,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { MainComponent } from './components/main/main.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CardBlockComponent } from './components/card-block/card-block.component';
+import { CardJobInfoComponent } from './components/card-job-info/card-job-info.component';
+import { CardJobRunsComponent } from './components/card-job-runs/card-job-runs.component';
+import { CardJobStatusComponent } from './components/card-job-status/card-job-status.component';
+import { CardBlockTwoComponent } from './components/card-block-two/card-block-two.component';
+import { CardJobListComponent } from './components/card-job-list/card-job-list.component';
+import { CardJobMetricsComponent } from './components/card-job-metrics/card-job-metrics.component';
 
 
 export function getAuthServiceConfigs() {
@@ -34,14 +43,23 @@ export function getAuthServiceConfigs() {
   declarations: [
     AppComponent,
     LoginComponent,
-    MainComponent
+    MainComponent,
+    SidebarComponent,
+    CardBlockComponent,
+    CardJobInfoComponent,
+    CardJobRunsComponent,
+    CardJobStatusComponent,
+    CardBlockTwoComponent,
+    CardJobListComponent,
+    CardJobMetricsComponent
   ],
   imports: [
     SocialLoginModule,
     BrowserModule,
     HttpModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [{
     provide: AuthServiceConfig,
