509052a6333c80632049b92e5d0d732e86351eca
Martynas <martynas@outlook.com>
2020-03-23 20:19:19 +0100

    ADD: job status component
    - will show the status of selected job

---
+++ src/webapp/client/src/app/components/card-job-status/card-job-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-job-status',
+  templateUrl: './card-job-status.component.html',
+  styleUrls: ['./card-job-status.component.css']
+})
+export class CardJobStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
