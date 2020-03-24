b84796f703f4b6074a399ef84ff81cbf1cc59e73
James McDermott <james.mcdermott7@mail.dcu.ie>
2020-03-24 01:15:41 +0000

    FIX: name of functions in failure messages

--- src/odin-engine/scheduler/handleStrings_test.go
+++ src/odin-engine/scheduler/handleStrings_test.go
@@ -14,7 +14,7 @@ func TestStripChairs(t *testing.T) {
     for i, testCase := range cases {
         t.Run(fmt.Sprintf(" %s.strip(%s) ", testCase.A, testCase.B), func(t *testing.T) {
             actual := stripChars(testCase.A, testCase.B)
-	    if (actual != testCase.Expected) {t.Errorf("TestCompare %d failed - expected: '%v' got: '%v'", i+1, actual, testCase.Expected)}
+	    if (actual != testCase.Expected) {t.Errorf("TestStripChairs %d failed - expected: '%v' got: '%v'", i+1, actual, testCase.Expected)}
         })
     }
 }
@@ -29,7 +29,7 @@ func TestSplitOnKeyword(t *testing.T) {
     for i, testCase := range cases {
         t.Run(fmt.Sprintf(" %s.splitOn(%s) ", testCase.A, testCase.B), func(t *testing.T) {
             actual := len(splitOnKeyword(testCase.A, testCase.B))
-	    if (actual != testCase.Expected) {t.Errorf("TestCompare %d failed - expected: '%v' got: '%v'", i+1, actual, testCase.Expected)}
+	    if (actual != testCase.Expected) {t.Errorf("TestSplitOnKeyword %d failed - expected: '%v' got: '%v'", i+1, actual, testCase.Expected)}
         })
     }
 }
@@ -43,7 +43,7 @@ func TestTrimEdges(t *testing.T) {
     for i, testCase := range cases {
         t.Run(fmt.Sprintf(" %s.trimEdgesFrom(%s) ", testCase.A, testCase.B), func(t *testing.T) {
             actual := trimEdges(testCase.A, testCase.B)
-	    if (actual != testCase.Expected) {t.Errorf("TestCompare %d failed - expected: '%v' got: '%v'", i, actual, testCase.Expected)}
+	    if (actual != testCase.Expected) {t.Errorf("TestTrimEdges %d failed - expected: '%v' got: '%v'", i, actual, testCase.Expected)}
         })
     }
 }
--- src/odin-engine/scheduler/keywords_test.go
+++ src/odin-engine/scheduler/keywords_test.go
@@ -13,7 +13,7 @@ func TestGetValidKeywords(t *testing.T) {
     for i, testCase := range cases {
         t.Run(fmt.Sprintf("getValidKeywords()"), func(t *testing.T) {
             actual := len(strings.Split(getValidKeywords(), " "))
-	    if (actual != testCase.Expected) {t.Errorf("TestIsTimeValid %d failed - expected: '%v' got: '%v'", i+1, actual, testCase.Expected)}
+	    if (actual != testCase.Expected) {t.Errorf("TestGetValidKeywords %d failed - expected: '%v' got: '%v'", i+1, actual, testCase.Expected)}
         })
     }
 }
@@ -25,7 +25,7 @@ func TestGetDowMap(t *testing.T) {
     for i, testCase := range cases {
         t.Run(fmt.Sprintf("getDowMap()"), func(t *testing.T) {
             actual := len(getDowMap())
-	    if (actual != testCase.Expected) {t.Errorf("TestIsTimeValid %d failed - expected: '%v' got: '%v'", i+1, actual, testCase.Expected)}
+	    if (actual != testCase.Expected) {t.Errorf("TestTestGetDowMap %d failed - expected: '%v' got: '%v'", i+1, actual, testCase.Expected)}
         })
     }
 }
@@ -37,7 +37,7 @@ func TestGetDomMap(t *testing.T) {
     for i, testCase := range cases {
         t.Run(fmt.Sprintf("getDomMap()"), func(t *testing.T) {
             actual := len(getDomMap())
-	    if (actual != testCase.Expected) {t.Errorf("TestIsTimeValid %d failed - expected: '%v' got: '%v'", i+1, actual, testCase.Expected)}
+	    if (actual != testCase.Expected) {t.Errorf("TestTestGetDomMap %d failed - expected: '%v' got: '%v'", i+1, actual, testCase.Expected)}
         })
     }
 }
@@ -49,7 +49,7 @@ func TestGetMonMap(t *testing.T) {
     for i, testCase := range cases {
         t.Run(fmt.Sprintf("getMonMap()"), func(t *testing.T) {
             actual := len(getMonMap())
-	    if (actual != testCase.Expected) {t.Errorf("TestIsTimeValid %d failed - expected: '%v' got: '%v'", i+1, actual, testCase.Expected)}
+	    if (actual != testCase.Expected) {t.Errorf("TestTestGetMonMap %d failed - expected: '%v' got: '%v'", i+1, actual, testCase.Expected)}
         })
     }
 }
