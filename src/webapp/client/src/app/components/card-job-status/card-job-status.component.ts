887b738f6b12d6b1297b829e0f8281076ddb4288
Martynas <martynas@outlook.com>
2020-04-02 12:11:45 +0100

    ADD : Functionality to card job status component
    - will get info from services

--- src/webapp/client/src/app/components/card-job-status/card-job-status.component.ts
+++ src/webapp/client/src/app/components/card-job-status/card-job-status.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { JobsService } from '../../services/jobs.service';
+import { ObservabilityService } from '../../services/observability.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-card-job-status',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardJobStatusComponent implements OnInit {
 
-  constructor() { }
+  private selectedJob: any;
+  private jobSchedule: String;
+  private selectedJobObv: any;
+  private jobLastRunStatus: String;
+  private jobLastRunTime: any;
+  
+  constructor(
+    private jobsService: JobsService,
+    private observabilityService: ObservabilityService
+  ) { }
 
   ngOnInit() {
+    this.selectedJobObv = this.jobsService._selectedJobListener
+    this.selectedJobObv.subscribe(job => {
+      this.selectedJob = job;
+      this.observabilityService.jobRunStatus(this.selectedJob.id).subscribe((response) => {
+        try {
+          // translate status code to string 
+          if (response.message.status[0] == "2") {
+            this.jobLastRunStatus = "Success";
+          } else if (response.message.status[0] == "4"){
+            this.jobLastRunStatus = "Warning";
+          } else if (response.message.status[0] == "5"){
+            this.jobLastRunStatus = "Failure";
+          } else {
+            this.jobLastRunStatus = response.message;
+          }
+          // convert run timestamp to human friendly format
+          this.jobLastRunTime = new Date((Number(response.message.time)*1000));
+        } catch(err) {
+          this.jobLastRunStatus = "No job results found"
+          this.jobLastRunTime =  "No job results found"
+        }
+      })
+    })
   }
 
 }
