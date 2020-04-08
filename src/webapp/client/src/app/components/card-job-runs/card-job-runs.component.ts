e4e0acdeadd8e648bb7b5cb03ee165f266c6b392
Martynas <martynas@outlook.com>
2020-04-08 15:24:45 +0100

    ADD : changes to card job runs component
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be
    #    ADD      (new feature)
    #    FIX      (bug fix)
    #    DEL      (delete feature)
    #    REFACTOR (refactoring code)
    #    STYLE    (lint fixes, formatting, missing semi colons, etc; no code change)
    #    DOCS     (changes to documentation)
    #    TEST     (adding or refactoring tests)
    #    OTHER    (other, explain in the body)
    # --------------------
    #    Capitalize title line
    #    Do not end the title line with a period
    #    Separate title from body with a blank line
    #    Explain what and why
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------

--- src/webapp/client/src/app/components/card-job-runs/card-job-runs.component.ts
+++ src/webapp/client/src/app/components/card-job-runs/card-job-runs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { JobsService } from '../../services/jobs.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-card-job-runs',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-job-runs.component.css']
 })
 export class CardJobRunsComponent implements OnInit {
-
-  constructor() { }
+  private selectedJob: any;
+  private jobSchedule: String;
+  private selectedJobObv : any;
+  
+  constructor(
+    private jobsService: JobsService
+  ) { }
 
   ngOnInit() {
+    this.selectedJobObv = this.jobsService._selectedJobListener
+    this.selectedJobObv.subscribe(job => {
+      this.selectedJob = job;
+    });
   }
 
 }
