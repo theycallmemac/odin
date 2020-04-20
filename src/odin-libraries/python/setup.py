b118a1697b7187f8d648d26a2790f59861611f52
James McDermott <james.mcdermott7@mail.dcu.ie>
2020-04-20 21:12:09 +0000

    ADD: support python sdk as pyodin on pypi, now installable via pip

---
+++ src/odin-libraries/python/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'pyodin',
+  packages = ['pyodin'],
+  version = '0.1.5',
+  license='MIT',
+  description = 'A python package for gathering information in odin jobs',
+  url = 'https://gitlab.computing.dcu.ie/mcdermj7/2020-ca400-urbanam2-mcdermj7',
+  author = 'James McDermott',
+  author_email = 'james.mcdermott7@mail.dcu.ue',
+  install_requires=[
+          'ruamel.yaml',
+          'pymongo',
+      ],
+  classifiers=[
+    'Programming Language :: Python :: 3', 
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
