bfe49c539ab6b140ddbb92fecdc5ca5705c09deb
Martynas <martynas@outlook.com>
2020-04-27 20:52:32 +0100

    FIX: python SDK CI fail due to scope
    - global variables referenced before assignment fixed and should make SDK work as intended again

--- src/odin-libraries/python/pyodin/odin_logger.py
+++ src/odin-libraries/python/pyodin/odin_logger.py
@@ -2,27 +2,30 @@ from pymongo import errors, MongoClient
 from os import environ
 
 try:
-    client = MongoClient(environ.get('ODIN_MONGODB'), serverSelectionTimeoutMS=2000)
-    db = client['odin']
-    collection = db['observability']
-    clientSuccess = True
+    CLIENT = MongoClient(environ.get('ODIN_MONGODB'), serverSelectionTimeoutMS=2000)
+    DB = CLIENT['odin']
+    COLLECTION = DB['observability']
+    CLIENT_SUCCESS = True
 except:
-    clientSuccess = False
+    CLIENT_SUCCESS = False
+
 
 class OdinLogger:
     @classmethod
-    def log(self, type, desc, value, id, timestamp):
+    def log(cls, type, desc, value, id, timestamp):
+        global CLIENT_SUCCESS
         try:
-            if (clientSuccess): 
-                self.find_and_insert(collection, type, desc, value,  id, timestamp)
+            if CLIENT_SUCCESS:
+                cls.find_and_insert(COLLECTION, type, desc, value,  id, timestamp)
         except:
-            # if logging fails, check connection 
-            clientSuccess = self.check_connection(client)
+            # if logging fails, check connection
+            global CLIENT
+            CLIENT_SUCCESS = cls.check_connection(CLIENT)
 
-    
+
     @staticmethod
-    def find_and_insert(collection, type, desc, value, id, timestamp):
-        collection.update_one(
+    def find_and_insert(COLLECTION, type, desc, value, id, timestamp):
+        COLLECTION.update_one(
             {'id': str(id), 'desc':str(desc), 'type': str(type), 'timestamp': str(timestamp)},
             {'$set' : {
                 'type' : str(type),
