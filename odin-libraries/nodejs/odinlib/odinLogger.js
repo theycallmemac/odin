44635e60710a4e74ffe5e7f17d1ea9597045d515
James McDermott <james.mcdermott7@mail.dcu.ie>
2020-07-16 00:16:30 +0100

    FIX: formatting of Node.js SDK

--- odin-libraries/nodejs/odinlib/odinLogger.js
+++ odin-libraries/nodejs/odinlib/odinLogger.js
@@ -1,34 +1,34 @@
-const http = require('http')
+const http = require('http');
 
 class OdinLogger {
-    async log(type, desc, value, id, timestamp) {
-        const data = type + "," + desc + "," + value + "," + id + "," + timestamp
+  async log(type, desc, value, id, timestamp) {
+    const data = type + ',' + desc + ',' + value + ',' + id + ',' + timestamp;
 
-	const options = {
-	  host: 'localhost',
-	  port: 3939,
-	  path: '/stats/add',
-	  method: 'POST',
-	  headers: {
-	    'Content-Type': 'application/json',
-	    'Content-Length': data.length
-	  }
-	}
+    const options = {
+      host: 'localhost',
+      port: 3939,
+      path: '/stats/add',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
+      },
+    };
 
-	const request = http.request(options, (response) => {
-	  response.on('data', (d) => {
-	    process.stdout.write(d)
-	  })
-	})
+    const request = http.request(options, (response) => {
+      response.on('data', (d) => {
+        process.stdout.write(d);
+      });
+    });
 
-	request.on('error', (error) => {
-            return false
-	})
+    request.on('error', (error) => {
+      return false;
+    });
 
-	request.write(data)
-	request.end()
-        return true
-    }
+    request.write(data);
+    request.end();
+    return true;
+  }
 }
 
 module.exports.OdinLogger = OdinLogger;
